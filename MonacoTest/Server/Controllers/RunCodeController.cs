75a06189fe6ecce15cef4974f3a878a23eb9714f
hayoonleeMe <htdobad@gmail.com>
2023-04-02 17:58:09 +0900

    ClassHub - JudgeServer 와 동기화 되도록 업데이트

--- MonacoTest/Server/Controllers/RunCodeController.cs
+++ MonacoTest/Server/Controllers/RunCodeController.cs
@@ -14,23 +14,50 @@ namespace MonacoTest.Server.Controllers {
         // POST api/<RunCode>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] JudgeRequest request) {
-            Console.WriteLine("#문제 번호 : " + request.TaskId);
-            Console.WriteLine("#언어 : " + request.Language);
-            Console.WriteLine("#코드 : " + request.Code);
-
             // to Judge Server
             HttpClient Http = new HttpClient();
             var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
-            var response = await Http.PostAsync("https://localhost:7109/Judge", content);
+            var response = await Http.PostAsync("https://localhost:7135/Judge", content);
 
             if (response.IsSuccessStatusCode) {
                 Console.WriteLine("Data posted successfully");
 
                 JudgeResult? judgeResult = await response.Content.ReadFromJsonAsync<JudgeResult>();
                 if (judgeResult != null) {
-                    Console.WriteLine("IsCorrect : " + (judgeResult.IsCorrect ? "true" : "false"));
-                    Console.WriteLine("ExecutionTime (ms) : " + judgeResult.ExecutionTime);
-                    Console.WriteLine("MemoryUsage (KB) : " + judgeResult.MemoryUsage);
+                    switch (judgeResult.Result) {
+                        case JudgeResult.JResult.Accepted:
+                            Console.WriteLine("맞았습니다.");
+                            Console.WriteLine("실행 시간(ms) : " + judgeResult.ExecutionTime);
+                            Console.WriteLine("메모리 사용량(KB) : " + judgeResult.MemoryUsage);
+                            break;
+                        case JudgeResult.JResult.WrongAnswer:
+                            Console.WriteLine("틀렸습니다.");
+                            break;
+                        case JudgeResult.JResult.CompileError:
+                            Console.WriteLine("컴파일 에러");
+                            break;
+                        case JudgeResult.JResult.RuntimeError:
+                            Console.WriteLine("런타임 에러");
+                            break;
+                        case JudgeResult.JResult.TimeLimitExceeded:
+                            Console.WriteLine("시간 초과");
+                            break;
+                        case JudgeResult.JResult.MemoryLimitExceeded:
+                            Console.WriteLine("메모리 초과");
+                            break;
+                        case JudgeResult.JResult.PresentationError:
+                            Console.WriteLine("출력 형식 에러");
+                            break;
+                        case JudgeResult.JResult.OutputLimitExceeded:
+                            Console.WriteLine("출력 한도 초과");
+                            break;
+                        case JudgeResult.JResult.JudgementFailed:
+                            Console.WriteLine("채점 실패");
+                            break;
+                        case JudgeResult.JResult.Pending:
+                            Console.WriteLine("채점 미완료");
+                            break;
+                    }
                 } else {
                     Console.WriteLine("Result is null");
                 }
--- MonacoTest/Shared/JudgeRequest.cs
+++ MonacoTest/Shared/JudgeRequest.cs
@@ -1,9 +1,8 @@
 ﻿namespace MonacoTest.Shared {
     public class JudgeRequest {
-        public string? TaskId { get; set; }
+        // 문제를 식별할 수 있는 Id
+        public string? Id { get; set; }
+        // 에디터에 작성하여 제출한 코드
         public string? Code { get; set; }
-        public string? Language { get; set; }
-        public string[]? TestInputs { get; set; }
-        public string[]? TestOutputs { get; set; }
     }
 }
